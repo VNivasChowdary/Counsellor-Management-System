019490d44e4a7a1e55ce4a6e4ad3214e612ce792
VNivasChowdary <96186579+VNivasChowdary@users.noreply.github.com>
2024-06-22 10:11:58 +0530

    Update test.py

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ from boto3.dynamodb.conditions import Key, Attr
 
 demo_table =resource(
     'dynamodb',
-    aws_access_key_id="AKIAQ3EGSMC5X6KNJSAQ",
-    aws_secret_access_key="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"
+    aws_access_key_id="",
+    aws_secret_access_key=""
 ).Table('userdata')
 
 name = "nivas"
